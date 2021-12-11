f54ac4dbab5b24f7142d3dc41f06c20f5e5abd8c
Daniel Guarin <90803895+danielguaro@users.noreply.github.com>
2021-12-10 21:16:18 -0500

    delete

--- proyectoCADEL-/src/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-const {projectResolvers} = require('./Project')
-const {userResolvers} = require('./user')
-const {inscripcionResolvers} = require ('./Inscripcion')
-const {avancesResolvers} = require ('./Avance')
-const dateTime  = require('./datetime')
-
-const resolvers = [projectResolvers, userResolvers, inscripcionResolvers, avancesResolvers, dateTime]
-
-module.exports = {resolvers}
